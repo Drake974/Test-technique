15b0ea755696375d3d853a55c366b7f1982d2dff
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-03-16 13:26:52 +0400

    ajout du modele de creation d'utilisateur

---
+++ src/class/Connection.php
@@ -0,0 +1,15 @@
+<?php 
+//connexion bdd
+
+ class Connection{
+
+    public static function getPDO (): PDO
+    {
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=culturel;charset=utf8', 'root', '');
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+    
+
+ }
--- src/views/login.php
+++ src/views/login.php
@@ -1,19 +1,20 @@
+
 <?php require_once('../elements/header.php');?>
 <section>
 <div class="d-flex justify-content-center mt-5">
-    <form action="" method="POST" class="form-login border shadow p-4">
+    <form action="../models/registerUser.php" method="POST" class="form-login border shadow p-4">
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="floatingLastName" placeholder="Nom" required>
+            <input type="text" class="form-control" id="floatingLastName" placeholder="Nom" required name="identity">
             <label for="floatingLastName">Identifiant*</label>
         </div>
         
         <div class="form-floating mb-3">
-            <input type="password" class="form-control" id="floatingLogin" placeholder="****" required>
+            <input type="password" class="form-control" id="floatingLogin" placeholder="****" required name="password">
             <label for="floatingLogin">Mot de passe*</label>
         </div>
         <p>* Obligatoire</p>
         <div  class="text-center">
-        <button type="submit" class="btn btn-primary">Se connecter</button>
+        <button type="submit" class="btn btn-primary" name="register_login">Se connecter</button>
         </div>
         
     </form>
