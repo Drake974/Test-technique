b42e76f8adb0e7b62c3dc973f8dbc860c4c36958
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-03-18 09:39:41 +0400

    mise a jour de la dasboard

--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -2,11 +2,11 @@
 
 
 <section class="row">
-<div class="col-2 col-sm-12 dashboard d-flex flex-column justify-content-center align-items-center">
+<div class="col-md-2 col-sm-12 dashboard d-flex flex-column justify-content-center align-items-center">
 
 <?php require_once('../elements/sidebar.php'); ?>
 </div>
-<div class="col col-sm-12">
+<div class="col-md-10 col-sm-12">
 <h2 class="text-center mt-5">Centre de gestion</h2>
 
 </div>
