9d1d4864983f9fdf06af463cfee70ccb88c6a6af
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-03-21 10:09:56 +0400

    modification des require_once dans booking

--- src/views/bookingDashboard.php
+++ src/views/bookingDashboard.php
@@ -1,15 +1,16 @@
 <?php require_once(dirname(__DIR__).'/controllers/session/session.php'); ?>
 <?php require_once(dirname(__DIR__).'/elements/header.php');?>
-
+<?php require_once(dirname(__DIR__).'/models/bookingShowManagement.php');
+require_once(dirname(__DIR__).'/models/bookingManagement.php');
+require_once(dirname(__DIR__).'/models/showComputer.php');
+?>
 
 <section class="row">
 
 <div class="col">
 
 <?php
-require_once(dirname(__DIR__).'/models/bookingShowManagement.php');
-require_once(dirname(__DIR__).'/models/bookingManagement.php');
-require_once(dirname(__DIR__).'/models/showComputer.php');
+
 require_once(dirname(__DIR__).'/elements/modal/registerAdd.php');
 require_once(dirname(__DIR__).'/elements/modal/deleteBooking.php');
 require_once(dirname(__DIR__).'/elements/bookingManagement.php');
